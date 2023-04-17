6639b71d2f9b52f647fa372447910ea331a57de5
UriyRatinskiy <117594065+UriyRatinskiy@users.noreply.github.com>
2023-04-17 19:30:53 +0000

    Deploying to gh-pages from @ UriyRatinskiy/goit-js-hw-10@b7a57a35935c06a40cfc240622da9b22fe9d975e 🚀

--- index.cdb7cf58.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/all").then((t=>t.json()));
-//# sourceMappingURL=index.cdb7cf58.js.map
--- index.d4e18cc6.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/all").then((function(t){return t.json()}));
-//# sourceMappingURL=index.d4e18cc6.js.map
