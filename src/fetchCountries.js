2b6b2bfb0d81c40a187c54379609a65b239a48a8
UriyRatinskiy <117594065+UriyRatinskiy@users.noreply.github.com>
2023-04-18 18:03:45 +0300

    HW10 04

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,13 @@
-export default  function fetchCountries(name){
-const BASE_URL = "https://restcountries.com/v3.1/all";
-const URL = `${BASE_URL}?fields=name.official,capital,population,flags,languages`;
 
-return fetch(URL)
-.then((response) => response.json())
-.then((data) => {
-    // console.log("data", data)
-    insertContent(data.articles);
-})
-.catch((error) => { 
-    console.log("error", error);
-});
-};
+export default function fetchCountries(nameCountry) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    const URL = `${BASE_URL}/${nameCountry}?fields=name,capital,population,flags,languages`;
+    return fetch(URL).then(response => {
+        if (!response.ok) {
+            clearPreviousInfo();
+            throw new Error(response.status);
+        }
+        return response.json()
+    });
+}
+
--- src/index.js
+++ src/index.js
@@ -1,24 +1,106 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
 
-const listItem = (item) => `<li>
-<img src="${item.flags.svg}" alt="${item.flags.alt}">
-<h2>${item.name.official}</h2>
-<p>capital: ${item.capital}</p>
-<p>population: ${item.population}</p>
-<p>languages: ${item.languages}</p>
-</li>`;
-// console.log(listItem);
-const generateContent = (array) => array.reduce((acc, item) => acc + listItem(item), "");
-// console.log(generateContent);
-const insertContent = (array) => {
-    const result = generateContent(array);
-    countryInfo.insertAdjacentHTML("beforeend", result);
+const refs = getRefs();
+ function getRefs() {
+    return {
+        searchBox: document.querySelector('#search-box'),
+        listCountries: document.querySelector('.country-list'),
+        countryInfo: document.querySelector('.country-info'),
+    };
+}
+
+refs.searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(event) {
+    const searchingCountry = event.target.value.trim();
+
+    clearPreviousInfo();
+
+    if (searchingCountry === '') {
+        return;
+    };
+
+    fetchCountries(searchingCountry)
+        .then(country => {
+            if (country.length > 10) {
+                return Notify.info("Too many matches found. Please enter a more specific name.");
+            };
+            if (country.length <= 10 && country.length >= 2) {
+                return createMarkupForCountries(country);
+            };
+            if (country.length === 1) {
+                return createCardForCountry(country);
+            };
+        })
+        .catch(error => {
+            return Notify.failure("Oops, there is no country with that name");
+        });
+};
+
+function createMarkupForCountries(countriesArray) {
+    const markup = countriesArray.map(({ flags, name }) => {
+        const itemEl = document.createElement('li');
+        itemEl.classList.add('country-item');
+        const flagEl = document.createElement('img');
+        flagEl.classList.add('country-flag');
+        flagEl.src = flags.svg;
+        flagEl.alt = 'flag';
+        flagEl.width = '50';
+        const nameEl = document.createElement('h3');
+        nameEl.textContent = name.official;
+
+        itemEl.append(flagEl, nameEl);
+
+        return itemEl;
+    });
+    refs.listCountries.append(...markup);
+};
+
+function createCardForCountry(countryArray) {
+    const markup = countryArray.map(country => {
+        const { flags, name, capital, population, languages } = country;
+
+        const itemEl = document.createElement('div');
+        itemEl.classList.add('country-header');
+        const flagEl = document.createElement('img');
+        flagEl.classList.add('country-flag');
+        flagEl.src = flags.svg;
+        flagEl.alt = 'flag';
+        flagEl.width = '50';
+        const nameEl = document.createElement('h1');
+        nameEl.textContent = name.official;
+
+        const bForCapital = document.createElement('b');
+        bForCapital.textContent = 'Capital: ';
+        const capitalEl = document.createElement('p');
+        capitalEl.textContent = `${capital}`;
+        capitalEl.prepend(bForCapital);
+
+        const bForPopulation = document.createElement('b');
+        bForPopulation.textContent = 'Population: ';
+        const populationEl = document.createElement('p');
+        populationEl.textContent = `${population}`;
+        populationEl.prepend(bForPopulation);
+
+        const bForlanguages = document.createElement('b');
+        bForlanguages.textContent = 'Languages: ';
+        const languagesEl = document.createElement('p');
+        languagesEl.textContent = `${(languages.map(language => language.name)).join(', ')}`;
+        languagesEl.prepend(bForlanguages);
+
+        itemEl.append(flagEl, nameEl);
+
+        return refs.countryInfo.append(itemEl, capitalEl, populationEl, languagesEl);
+    });
+    refs.countryInfo.append(markup);
+};
+
+function clearPreviousInfo() {
+    refs.listCountries.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
 };
