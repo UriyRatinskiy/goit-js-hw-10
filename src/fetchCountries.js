d1886e57556a1af9c4e6f27a92f84e4dead5ee7b
UriyRatinskiy <117594065+UriyRatinskiy@users.noreply.github.com>
2023-04-19 17:21:31 +0300

    HW10 06

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
 
-export default function fetchCountries(nameCountry) {
+export default function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1/name';
-    const URL = `${BASE_URL}/${nameCountry}?fields=name,capital,population,flags,languages`;
+    const URL = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
     return fetch(URL).then(response => {
         if (!response.ok) {
-            
+            clearPreviousInfo()
             throw new Error(response.status);
         }
         return response.json()
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const refs = {
 refs.searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 
 function searchCountry(event) {
-    const searchingCountry = event.target.value.trim();
+    let searchingCountry = event.target.value.trim();
 
     clearPreviousInfo();
 
@@ -31,7 +31,7 @@ function searchCountry(event) {
             if (country.length <= 10 && country.length >= 2) {
                 return createMarkupForCountries(country);
             };
-            if (country.length === 1) {
+            if (country.length === 0) {
                 return createCardForCountry(country);
             };
         })
@@ -40,70 +40,104 @@ function searchCountry(event) {
         });
 };
 
+// function createMarkupForCountries(countriesArray) {
+//     const markup = countriesArray.map(({ flags, name }) => {
+//         const itemEl = document.createElement('li');
+//               itemEl.classList.add('country-item');
+
+//         const flagEl = document.createElement('img');
+//               flagEl.classList.add('country-flag');
+//               flagEl.src = flags.svg;
+//               flagEl.alt = 'flag';
+//               flagEl.width = '50';
+//               flagEl.height = '30';
+
+//         const nameEl = document.createElement('h2');
+//               nameEl.textContent = name.official;
+
+//         itemEl.append(flagEl, nameEl);
+
+//         return itemEl;
+//     });
+//     refs.listCountries.append(...markup);
+// };
 function createMarkupForCountries(countriesArray) {
-    const markup = countriesArray.map(({ flags, name }) => {
-        const itemEl = document.createElement('li');
-              itemEl.classList.add('country-item');
+        const markup = countriesArray.map(item => {
+            return`<li class="country-item">
+        <img class="country-flag" src="${item.flags.svg}" alt="${item.flags.svg}" width = "50" height = "30">
+        <h2>${item.name.official}</h2> 
+        </li>
+         `;
+        })
+   .join('');
+   return markup;
+    }
 
-        const flagEl = document.createElement('img');
-              flagEl.classList.add('country-flag');
-              flagEl.src = flags.svg;
-              flagEl.alt = 'flag.alt';
-              flagEl.width = '50';
-              flagEl.height = '30';
+    function createCardForCountry(countriesArray) {
+        const markup = countriesArray.map(item => {
+            return`<div class="country-header">
+        <img class="country-flag" src="${item.flags.svg}" alt="${item.flags.svg}" width = "50" height = "30">
+        <h2>${item.name.official}</h2> 
+        <p><b>Capital:</b>&nbsp;${item.capital}</p>
+        <p><b>Population:</b>&nbsp;${item.population}</p>
+        <p><b>Languages:</b>&nbsp;${item.languages}</p>
+        </div>
+         `;
+        })
+   .join('');
+   return markup;
+    }
 
-        const nameEl = document.createElement('h2');
-              nameEl.textContent = name.official;
+    const createMarkup = { createMarkupForCountries, createCardForCountry };
 
-        itemEl.append(flagEl, nameEl);
+    const insertContent = (array) => {
+        const result = createMarkup.createMarkupForCountries(countriesArray);
+        listCountries.insertAdjacentHTML("beforeend", result);
+    }
 
-        return itemEl;
-    });
-    refs.listCountries.append(...markup);
-};
+// function createCardForCountry(countryArray) {
+//     const markup = countryArray.map(country => {
+//         const { flags, name, capital, population, languages } = country;
 
-function createCardForCountry(countryArray) {
-    const markup = countryArray.map(country => {
-        const { flags, name, capital, population, languages } = country;
+//         const itemEl = document.createElement('div');
+//               itemEl.classList.add('country-header');
 
-        const itemEl = document.createElement('div');
-              itemEl.classList.add('country-header');
+//         const flagEl = document.createElement('img');
+//               flagEl.classList.add('country-flag');
+//               flagEl.src = flags.svg;
+//               flagEl.alt = 'flag';
+//               flagEl.width = '50';
+//               flagEl.height = '30';
 
-        const flagEl = document.createElement('img');
-              flagEl.classList.add('country-flag');
-              flagEl.src = flags.svg;
-              flagEl.alt = 'flag.alt';
-              flagEl.width = '50';
-              flagEl.height = '30';
+//         const nameEl = document.createElement('h1');
+//               nameEl.textContent = name.official;
 
-        const nameEl = document.createElement('h1');
-              nameEl.textContent = name.official;
+//         const bForCapital = document.createElement('b');
+//               bForCapital.textContent = 'Capital: ';
+//         const capitalEl = document.createElement('p');
+//               capitalEl.textContent = `${capital}`;
+//               capitalEl.prepend(bForCapital);
 
-        const bForCapital = document.createElement('b');
-              bForCapital.textContent = 'Capital: ';
-        const capitalEl = document.createElement('p');
-              capitalEl.textContent = `${capital}`;
-              capitalEl.prepend(bForCapital);
+//         const bForPopulation = document.createElement('b');
+//               bForPopulation.textContent = 'Population: ';
+//         const populationEl = document.createElement('p');
+//               populationEl.textContent = `${population}`;
+//               populationEl.prepend(bForPopulation);
 
-        const bForPopulation = document.createElement('b');
-              bForPopulation.textContent = 'Population: ';
-        const populationEl = document.createElement('p');
-              populationEl.textContent = `${population}`;
-              populationEl.prepend(bForPopulation);
+//         const bForlanguages = document.createElement('b');
+//              bForlanguages.textContent = 'Languages: ';
+//         const languagesEl = document.createElement('p');
+//               languagesEl.textContent = `${(languages.map(language => language.name)).join(', ')}`;
+//               languagesEl.prepend(bForlanguages);
 
-        const bForlanguages = document.createElement('b');
-             bForlanguages.textContent = 'Languages: ';
-        const languagesEl = document.createElement('p');
-              languagesEl.textContent = `${(languages.map(language => language.name)).join(', ')}`;
-              languagesEl.prepend(bForlanguages);
+//         itemEl.append(flagEl, nameEl);
 
-        itemEl.append(flagEl, nameEl);
-
-        return refs.countryInfo.append(itemEl, capitalEl, populationEl, languagesEl);
-    });
-    refs.countryInfo.append(markup);
-};
-
+//         return refs.countryInfo.append(itemEl, capitalEl, populationEl, languagesEl);
+//         console.log(countryArray);
+//     });
+//     refs.countryInfo.append(markup);
+// };
+// console.log(countryArray);
 function clearPreviousInfo() {
     refs.listCountries.innerHTML = '';
     refs.countryInfo.innerHTML = '';
